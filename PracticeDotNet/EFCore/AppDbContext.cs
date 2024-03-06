5f5bcb956a6541db887368c5758be6e33459f801
Chaw Su Han <chawsuhan.csh415@gmail.com>
2024-03-06 09:48:39 +0630

    EFCore

---
+++ PracticeDotNet/EFCore/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticeDotNet.EFCore
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder SqlConnection = new SqlConnectionStringBuilder()
+            {
+                DataSource=".",
+                InitialCatalog="C#TestDb",
+                UserID="sa",
+                Password="sasa@123"
+            };
+            optionsBuilder.UseSqlServer(SqlConnection.ConnectionString);
+        }
+    }
+}
